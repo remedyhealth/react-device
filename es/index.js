24532f710400ad945ccb419d50fb7994b8b0aa66
Tom Jenkins <tjenkins@remedyhealthmedia.com>
2017-02-17 17:23:29 -0500

    add the es and lib directories to the git repo

---
+++ es/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SIZE_UNKNOWN = exports.PORTRAIT = exports.LANDSCAPE = undefined;
+
+var _Device = require('./Device');
+
+var _Device2 = _interopRequireDefault(_Device);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Device2.default;
+exports.LANDSCAPE = _Device.LANDSCAPE;
+exports.PORTRAIT = _Device.PORTRAIT;
+exports.SIZE_UNKNOWN = _Device.SIZE_UNKNOWN;
