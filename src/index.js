279707859284d7a5465c8f7edb99b570205aa8dc
Tom Jenkins <tjenkins@remedyhealthmedia.com>
2017-02-20 16:32:54 -0500

    fix case sensitive filename/import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Device, { LANDSCAPE, PORTRAIT, SIZE_UNKNOWN } from './Device'
+import Device, { LANDSCAPE, PORTRAIT, SIZE_UNKNOWN } from './device'
 
 export default Device
 export { LANDSCAPE, PORTRAIT, SIZE_UNKNOWN }
