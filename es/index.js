ba010d3e24d2907402a2340ef93598ba289c8595
Tom Jenkins <tjenkins@remedyhealthmedia.com>
2017-03-08 15:13:19 -0500

    readd es & lib dirs

---
+++ es/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SIZE_UNKNOWN = exports.PORTRAIT = exports.LANDSCAPE = undefined;
+
+var _device = require('./device');
+
+var _device2 = _interopRequireDefault(_device);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _device2.default;
+exports.LANDSCAPE = _device.LANDSCAPE;
+exports.PORTRAIT = _device.PORTRAIT;
+exports.SIZE_UNKNOWN = _device.SIZE_UNKNOWN;
