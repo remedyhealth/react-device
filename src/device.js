cf6722a6e305fb307c901c112a50026e17b89910
Tom Jenkins <tjenkins@remedyhealthmedia.com>
2017-03-08 10:16:23 -0500

    fix server side handling
    ComponentDidMount is not called on server side which is fine when adding
    an window event listener. However ComponentDidMount was when the newly
    added Device onChange was called. So moved the initial call of onChange
    to the constructor so callers can get the event on the server side.
    
    That also required separating the publish function from the onChange
    handler. This way I can send just one listener to get the device info.

--- src/device.js
+++ src/device.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import debounce from 'lodash/debounce'
 import parser from 'ua-parser-js'
 
-export const SIZE_UNKNOWN = -1
+export const SIZE_UNKNOWN = null
 export const LANDSCAPE = 'landscape'
 export const PORTRAIT = 'portrait'
 
@@ -34,28 +34,34 @@ class Device extends Component {
   }
 
   static _onChange() {
+    Device._publish(Device._listeners)
+  }
+
+  static _publish(listeners) {
     const deviceInfo = Device._buildDeviceInfo()
-    Device._listeners.forEach(listener => {
+    listeners.forEach(listener => {
       listener(deviceInfo)
     })
   }
 
   static _listeners = []
-  static DEBOUNCE_TIME = 100
+  static DEBOUNCE_TIME = 250
   static details = {}
 
   constructor (props) {
     super(props)
     Device.details = parser(props.userAgent)
+    if (!Device._debouncedChange) {
+      Device._debouncedChange = debounce(Device._onChange, Device.DEBOUNCE_TIME)
+    }
+    Device._publish([props.onChange])
   }
 
   componentDidMount () {
     if (!Device._listeners.length) {
-      Device._debouncedChange = debounce(Device._onChange, Device.DEBOUNCE_TIME)
       window.addEventListener('resize', Device._debouncedChange, true)
     }
     Device._listeners.push(this.props.onChange)
-    Device._debouncedChange()
   }
 
   componentWillUnmount () {
